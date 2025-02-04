bc1238227dcffb89bf076cedbcc2f9a2be770009
nick <nicole.martins@unimedpalmas.com.br>
2025-02-04 16:44:38 -0300

    carrossel bemfeito feito

--- src/app/carrossel/carrossel.tsx
+++ src/app/carrossel/carrossel.tsx
@@ -1,35 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./carrossel.css";
 
+const images = [
+  'https://wallpapers.com/images/hd/3840x1080-pictures-mweskuy056nv0cjr.jpg',
+  'https://wallpapers.com/images/hd/3840x1080-pictures-mweskuy056nv0cjr.jpg',
+  'https://wallpapers.com/images/hd/3840x1080-pictures-mweskuy056nv0cjr.jpg'
+];
 
-  const images = [
-    'https://wallpapers.com/images/hd/3840x1080-pictures-mweskuy056nv0cjr.jpg',
-    'https://wallpapers.com/images/hd/3840x1080-pictures-mweskuy056nv0cjr.jpg',
-    'https://wallpapers.com/images/hd/3840x1080-pictures-mweskuy056nv0cjr.jpg'
-  ];
-  
-  const Carousel: React.FC = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-  
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-      }, 7000);
-      return () => clearInterval(interval);
-    }, []);
-  
-    return (
-      <div className="carousel-container">
+const Carousel: React.FC = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 7000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="carousel-container">
+      <div
+        className="carousel-track"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
         {images.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`Imagem ${index + 1}`}
-            className={`carousel-image ${index === currentIndex ? "active" : ""}`}
-          />
+          <img key={index} src={image} alt={`Imagem ${index + 1}`} className="carousel-image" />
         ))}
       </div>
-    );
-  };
-  
-  export default Carousel;
+    </div>
+  );
+};
+
+export default Carousel;
