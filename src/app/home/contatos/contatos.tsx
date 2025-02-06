86b41efce8ca47add980ec45b9e776bc8c1e4025
nick <nicole.martins@unimedpalmas.com.br>
2025-02-06 10:12:53 -0300

    inserção dos serviços

--- src/app/home/contatos/contatos.tsx
+++ src/app/home/contatos/contatos.tsx
@@ -14,28 +14,27 @@ import Baner from './vemserunimed.png';
 export default function Contatos() {
     return (
         <div className="container-contatos">
-            <li>
+            <li className="contatos">
             <Image src= {Contato} alt="contato" height={600} width={600}>
             </Image>
             </li>
-            <li><a href="https://www.unimed.coop.br/site/web/blumenau/sos-unimed"  target="_blank" rel="noopener noreferrer">
+            <li className="sos"><a href="https://www.unimed.coop.br/site/web/blumenau/sos-unimed"  target="_blank" rel="noopener noreferrer">
                 <Image src= {Sos} alt="sos" height={280} width={280}>
                 </Image>
             </a></li>
-            <li><a href="https://api.whatsapp.com/send/?phone=47999010459&text=Ol%C3%A1&app_absent=0" target="_blank" rel="noopener noreferrer">
+            <li className="whats"><a href="https://api.whatsapp.com/send/?phone=47999010459&text=Ol%C3%A1&app_absent=0" target="_blank" rel="noopener noreferrer">
                 <Image src= {Whats} alt="whats" height={280} width={280}>
                 </Image>
             </a></li>
-            <li><a href="https://www.unimed.coop.br/site/web/08006470026" target="_blank" rel="noopener noreferrer">
+            <li className="sac"><a href="https://www.unimed.coop.br/site/web/08006470026" target="_blank" rel="noopener noreferrer">
                 <Image src= {Sac} alt="sac" height={280} width={280}>
                 </Image>
             </a></li>
-            <li><a href="https://www.unimed.coop.br/site/web/blumenau/contrate-seu-plano" target="_blank" rel="noopener noreferrer">
-                <Image src= {Contratar} alt="contratar" height={280} width={280}>
-                </Image>
+            <li className="contratar"><a href="https://www.unimed.coop.br/site/web/blumenau/contrate-seu-plano" target="_blank" rel="noopener noreferrer">
+                <Image src= {Contratar} alt="contratar" height={280}/>
             </a></li>
-            <li className="baner"><a href="https://platform.senior.com.br/hcmrs/hcm/curriculo/?tenant=unimedblumenau&tenantdomain=unimedblumenau.com.br#!/vacancies/list" target="_blank" rel="noopener noreferrer">
-                <Image src= {Baner} alt="contratar">
+            <li className="baner"><a href="https://unimedpalmas.vagas.solides.com.br/" target="_blank" rel="noopener noreferrer">
+                <Image src= {Baner} alt="contratar" height={1000}>
                 </Image>
             </a></li>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@
 import React, {useState} from "react";
 import Image from "next/image";
 import Navbar from "./components/navbar/navbar"
-import ImageCarousel from "./carrossel/carrossel";
-import Contatos from "./contatos/contatos";
-import Fundo from "./fundo/fundo";
+import ImageCarousel from "./home/carrossel/carrossel";
+import Contatos from "./home/contatos/contatos";
+import Fundo from "./home/fundo/fundo";
+import Servicos from "./servicos/servicos";
 
 export default function Home() {
   return (
@@ -22,8 +23,10 @@ export default function Home() {
       <div className="fundo">
         <Fundo />
       </div>
-    </div>
-  
 
+      <div className="servicos">
+        <Servicos />
+      </div>
+    </div>
   );
 }
