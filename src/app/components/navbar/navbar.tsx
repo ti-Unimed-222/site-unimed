1ddff5dbfac4a33c7f8f837d72389e5abfca082b
nick <nicole.martins@unimedpalmas.com.br>
2025-02-07 11:55:43 -0300

    navbar pronta

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -5,42 +5,55 @@ import React, { useState } from 'react';
 import './Navbar.css';
 import logo from "./logo.png"
 import Image from 'next/image';
-
-import Button from './click';
+import Numero from './contato.png'
+import { Search } from 'lucide-react';
+import { User } from 'lucide-react';
+import { MapPin } from 'lucide-react'; 
 
 export default function Navbar() {
     const [searchTerm, setSearchTerm] = React.useState("");
   
     return (
       <nav className="navbar">
-        <div className="container mx-auto flex justify-between items-center p-4">
-          {/* Logo */}
-          <div className="flex items-center gap-4">
-            <Image src = {logo} alt="Logo" height={170} width={170}/>
-            <div className='navbar-link'>
-            <ul className="flex space-x-6 text-gray font-medium">
-          
+        <div className="logo">
+              <Image src = {logo} alt="Logo" height={170} width={170}/>
+            </div>
+        <div className="nav-container">
+          <div className='primeira-nav'>
+            <div className='numero'>
+              <Image src={Numero} alt='numero'/>
+            </div>
+          <div className='bots'>
+            <div className='busca-container'>
+            <Search className="search-icon" />
+              <input className='search'
+                type="text"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                placeholder="Faça sua busca aqui"
+              />
+            </div>
+
+              
+              <button className="login">
+              <User className='usericon' />
+                Login
+              </button>
+              </div>
+              
+          </div>
+
+          <div className='navbar-link'>
             <li><a href="#home">Unimed Palmas</a></li>
             <li><a href="#servicos" >Serviços</a></li>
             <li><a href="#programas">Programas</a></li>
             <li><a href="#planos">Planos</a></li>
-          </ul>
-          </div>
-          </div>
-          
-            {/* Search & Login */}
-
-            <input className='search'
-              type="text"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              placeholder="Busca"
-            />
-            <button className="login">
-              Login
-            </button>
+            <div className='local'>
+              <MapPin className='localicon' />
+              <h1 className='localtext'><a href='https://www.google.com/maps?q=-10.2089709,-48.3350843' target='_blank' rel='noopener noreferrer'>NOSSA LOCALIZAÇÃO</a></h1>
             </div>
-
+          </div>   
+        </div>
       </nav>
     );
   }
---
+++ src/app/home/servicos/servicos.tsx
@@ -0,0 +1,53 @@
+'use client';
+import Image from "next/image";
+import Baner from './banner.png';
+import Boleto from './icones/boleto.png'
+import Guia from './icones/guia.png'
+import Plano from './icones/plano.png'
+import Portal from './icones/portal.png'
+import Fale from './icones/fale.png'
+import Atendimento from './icones/deficiente.png'
+import './servicos.css'
+
+export default function Servicos () {
+    return (
+        <div className="servicos-container">
+            <div className="baner">
+                <Image src={Baner} alt="baner" width={10000}/>
+            </div>
+            <div className="services-items">
+                <li className="boleto">
+                <a href="https://boleto.unimedpalmas.coop.br/" target="_blank" rel="noopener noreferrer">
+                    <Image src={Boleto} alt="boleto" />
+                </a>
+                </li>
+                <li className="guia">
+                <a href="https://portal.unimedpalmas.coop.br/pls_paginaGuiaMedico.jsp" target="_blank" rel="noopener noreferrer">
+                    <Image src={Guia} alt="guia" />
+                </a>
+                </li>
+                <li className="plano">
+                <a href="https://portal.unimedpalmas.coop.br/pls_paginaGuiaMedico.jsp" target="_blank" rel="noopener noreferrer">
+                    <Image src={Plano} alt="plano" />
+                </a>
+                </li>
+                <li className="portal">
+                <a href="https://portal.unimedpalmas.coop.br/" target="_blank" rel="noopener noreferrer">
+                    <Image src={Portal} alt="portal" />
+                </a>
+                </li>
+                <li className="fale">
+                <a href="#ouvidoria">
+                    <Image src={Fale} alt="fale" />
+                </a>
+                </li>
+                <li className="atendimento">
+                <a href="#atendimento">
+                    <Image src={Atendimento} alt="atendimento" />
+                </a>
+                </li>
+            </div>
+        </div>
+
+    );
+}
